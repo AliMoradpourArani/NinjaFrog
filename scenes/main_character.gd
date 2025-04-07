05a0393a6f3e31e6ba744b284bdbdc9dc6ee58bd
Heart-Ali <aaammm.moradali@gmail.com>
2025-04-07 10:43:18 +0330

    Add animations

--- scenes/main_character.gd
+++ scenes/main_character.gd
@@ -3,12 +3,19 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+@onready var sprite_2d = $Sprite2D
 
 
 func _physics_process(delta):
+	# Animations
+	if (velocity.x > 1 or velocity.x < -1):
+		sprite_2d.animation = "running"
+	else:
+		sprite_2d.animation = "default"
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		sprite_2d.animation = "jumping"
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -20,6 +27,9 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 12)
 
 	move_and_slide()
+	
+	var isLeft = velocity.x < 0
+	sprite_2d.flip_h = isLeft
